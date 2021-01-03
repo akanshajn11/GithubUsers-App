ba6108a4aaf914c0cdbae6bd2462bc67c251e9d1
Akansha Jain <akansha.jn111@gmail.com>
2021-01-03 14:11:56 +0530

    Menu and favorite icon

--- app/src/main/java/com/example/users/detail/DetailActivity.kt
+++ app/src/main/java/com/example/users/detail/DetailActivity.kt
@@ -2,14 +2,19 @@ package com.example.users.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.users.R
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_detail)
 
         val username: TextView = findViewById(R.id.username)
@@ -23,6 +28,18 @@ class DetailActivity : AppCompatActivity() {
         Glide.with(this)
             .load(intent.extras?.getString("avatarUrl"))
             .into(userImage)
-
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.detail_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        item.icon = ContextCompat.getDrawable(this, R.drawable.ic_baseline_favorite_24)
+        Toast.makeText(this, "Added to favorites", Toast.LENGTH_SHORT).show()
+        return super.onOptionsItemSelected(item)
+    }
+
 }
